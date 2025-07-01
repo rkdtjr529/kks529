976546983654b7b0c888573ed068ba569128c560
kks529 <rkdtjr52@gmail.com>
2025-07-01 14:31:48 +0900

    First Commit

---
+++ Character/Player/C_SwordMan.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/Player/C_PlayerCharacter.h"
+#include "C_SwordMan.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLETEST_API AC_SwordMan : public AC_PlayerCharacter
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
